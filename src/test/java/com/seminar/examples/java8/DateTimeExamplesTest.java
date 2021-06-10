67e1659d14795358a215f694e1ced2390d831b29
Viviana Sarmiento <viviana.sarmiento@rubiconwater.com>
2021-06-10 18:18:08 +1000

    renamed folder

--- src/test/java/com/seminar/examples/java8/DateTimeExamplesTest.java
+++ src/test/java/com/seminar/examples/java8/DateTimeExamplesTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.neiljbrown.examples.java8;
+package com.seminar.examples.java8;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.lessThan;
--- src/test/java/com/seminar/examples/java8/StreamApiExamplesTest.java
+++ src/test/java/com/seminar/examples/java8/StreamApiExamplesTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.neiljbrown.examples.java8;
+package com.seminar.examples.java8;
 
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.mapping;
